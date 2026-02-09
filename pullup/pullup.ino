15e006b7c1e8a5a9d1549c47991ae3c86bb8b29e
minho <pmh9038@naver.com>
2026-02-09 14:43:56 +0900

    pull up code add

--- pullup/pullup.ino
+++ pullup/pullup.ino
@@ -4,13 +4,14 @@ const int buttonPin = 2;
 int buttonState = 0;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin);
-
+  Serial.println(buttonState);
   if(buttonState == LOW){
       digitalWrite(ledPin, HIGH); 
   } else {
