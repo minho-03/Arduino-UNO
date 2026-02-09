b0ade7a786cd7a8b2cd3b58d3f057e5645f44fcc
minho <pmh9038@naver.com>
2026-02-09 16:54:51 +0900

    초음파센서로 거리측정 및 LED 속도제어

--- Distance_LED/Distance_LED.ino
+++ Distance_LED/Distance_LED.ino
@@ -1,9 +1,9 @@
 int ledPin = 13;
-int trigPin = 9;
+int trigPin = 9; 
 int echoPin = 10;
 
-long duration;
-int distance;
+long hh; // 시간
+int distance; // 거리
 
 void setup() {
   Serial.begin(9600);
@@ -22,10 +22,10 @@ void loop() {
   digitalWrite(trigPin, LOW);
 
   // 반사파 시간 측정
-  duration = pulseIn(echoPin, HIGH);
+  hh = pulseIn(echoPin, HIGH);
 
   // 거리 계산 (cm)
-  distance = duration * 0.034 / 2;
+  distance = hh * 0.034 / 2;
 
   Serial.print("Distance: ");
   Serial.print(distance);
