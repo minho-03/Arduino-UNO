e909aec3c0d91fb40ed615cbf4925c7dd4b92608
minho <pmh9038@naver.com>
2026-02-09 16:37:39 +0900

    mod

--- analog_input/analog_input.ino
+++ analog_input/analog_input.ino
@@ -3,10 +3,14 @@ int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
 void setup() {
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  delay(1);
   sensorValue = analogRead(sensorPin);
   digitalWrite(ledPin, HIGH);
   delay(sensorValue);
