bcad15ac11c50597ea86e429c59c615af95a5995
minho <pmh9038@naver.com>
2026-02-10 10:46:40 +0900

    LED밝기 조절 코드 추가

--- Analog_Voltage/Analog_Voltage.ino
+++ Analog_Voltage/Analog_Voltage.ino
@@ -1,9 +1,16 @@
+int ledPin = 9;
+int analogPin = A3;
+int val = 0;
+
 void setup() {
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   int sensorValue = analogRead(A0);
   float voltage = sensorValue * (5.0 / 1023.0);
+  val = analogRead(analogPin);
+  analogWrite(ledPin, val / 4);
   Serial.println(voltage);
 }
