09712fb5e033509cfe9f00960743d7d8c4bd3cd4
minho <pmh9038@naver.com>
2026-02-10 12:45:34 +0900

    code add

---
+++ servo_RGB/servo_RGB.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int potPin = A0;
+int servoPin = 9;
+
+int redPin = 3;
+int greenPin = 5;
+
+int val;      
+int angle;    
+
+void setup() {
+  myservo.attach(servoPin);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+}
+
+void loop() {
+  val = analogRead(potPin);
+  angle = map(val, 0, 1023, 0, 180);
+  myservo.write(angle);
+
+  if (angle >= 30 && angle <= 150) {
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(redPin, LOW);
+  } else {
+
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+  }
+
+  delay(15);
+}
